73748614d36617cade8c1997a30a00720f2499c5
焦哥 <jiaoyingjun@quancheng-ec.com>
2017-05-23 22:46:14 +0800

    fix

--- src/main/java/eurekademo/EurekaApplication.java
+++ src/main/java/eurekademo/EurekaApplication.java
@@ -5,16 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
- *
  * @author Gunnar Hillert
- *
  */
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaApplication {
 
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(EurekaApplication.class, args);
-	}
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
 
 }
